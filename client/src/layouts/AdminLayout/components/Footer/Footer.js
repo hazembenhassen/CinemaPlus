1b6efda51743878003ad69628850a648dee9830b
hazem <hazem.benhassen@isimg.tn>
2025-01-19 20:24:20 +0100

    commit

---
+++ client/src/layouts/AdminLayout/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Divider, Typography, Link } from '@material-ui/core';
+import useStyles from './styles';
+
+export default function Footer() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Divider />
+    
+      <Typography variant="caption">
+     
+      </Typography>
+    </div>
+  );
+}
